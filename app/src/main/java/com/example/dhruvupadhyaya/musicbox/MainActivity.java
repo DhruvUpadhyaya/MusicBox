9125433268b44b624f5153c6680c97c203895d9a
DhruvUpadhyaya <40164639+DhruvUpadhyaya@users.noreply.github.com>
2018-07-25 02:19:23 +0530

    Initial commit!!

---
+++ app/src/main/java/com/example/dhruvupadhyaya/musicbox/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.dhruvupadhyaya.musicbox;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+
+}
